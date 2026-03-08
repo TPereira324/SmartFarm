f63ba7bb09cc5a83920cc77edcb9940242aaaf4f
Fábio Rómulo <fabioromulo19@gmail.com>
2026-03-08 19:37:40 +0000

    provavel conexao com a bd

--- Back-end/index.php
+++ Back-end/index.php
@@ -1,11 +1,2 @@
 <?php
-
-require_once 'models/Model.php';
-require_once 'views/View.php';
-require_once 'controllers/Controller.php';
-
-$model = new Model();
-$view = new View();
-$controller = new Controller($model, $view);
-
-$controller->handleRequest();
+echo "Ola";
